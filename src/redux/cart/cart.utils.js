c90d3931b61b64fb9c438a90429a8500cb02d361
vikash.patel1 <p.vikash2810@gmail.com>
2023-01-28 16:13:12 -0500

    removed object export and exported as named

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,5 +1,3 @@
-import CartItem from "../../components/cart-item/cart-item.component";
-
 export const addItemToCart = (cartItems, cartItemToadd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === cartItemToadd.id
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,11 +5,11 @@ const { createStore, applyMiddleware } = require("redux");
 const { default: rootReducer } = require("./root-reducer");
 
 const middlewares = [];
-if(process.env.NODE_ENV === 'development') {
-    middlewares.push(logger)
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persister = persistStore(store);
 
-export default { store, persister };
+// export default { store, persister };
